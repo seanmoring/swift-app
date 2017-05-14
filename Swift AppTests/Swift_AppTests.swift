4818856928abefb3a85488265903cb721eb1be0a
Sean Moring <seanmoring@seans-air.hitronhub.home>
2017-05-14 15:18:22 -0600

    Initial Commit

---
+++ Swift AppTests/Swift_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Swift_AppTests.swift
+//  Swift AppTests
+//
+//  Created by Sean Moring on 2017-05-14.
+//  Copyright © 2017 Sean Moring. All rights reserved.
+//
+
+import XCTest
+@testable import Swift_App
+
+class Swift_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
